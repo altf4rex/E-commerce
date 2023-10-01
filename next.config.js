9fb9d61b4e0d4e3c74045bd2a3cc7f1c448f8d7d
altf4rex <mr.stereo2002@gmail.com>
2023-10-01 21:01:31 +0300

    img

--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ const nextConfig = {}
 module.exports = {
   images: {
     domains: ['ecom-branch.pockethost.io'], 
+    imageSizes: [16, 32, 48, 64],
+    loader: 'default', 
+    path: '/api/files/products/', 
   },
 }
--- utils/toUrl.tsx
+++ utils/toUrl.tsx
@@ -2,5 +2,5 @@
 const toUrl = (id: string, url?: string) => {
   return `https://ecom-branch.pockethost.io/api/files/products/${id}/${url}`
 }
-
+console.log(toUrl)
 export default toUrl
