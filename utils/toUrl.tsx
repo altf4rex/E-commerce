9a169e0a5294338e1fc7d54065570dcc555baa76
altf4rex <mr.stereo2002@gmail.com>
2023-10-01 20:24:26 +0300

    img

--- utils/toUrl.tsx
+++ utils/toUrl.tsx
@@ -1,6 +1,6 @@
 
 const toUrl = (id: string, url?: string) => {
-  return `http://127.0.0.1:8090/api/files/products/${id}/${url}`
+  return `https://ecom-branch.pockethost.io/api/files/products/${id}/${url}`
 }
 
 export default toUrl
